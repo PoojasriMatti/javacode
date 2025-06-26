acda385e119ac95cddbd02252da035e5096055fc
PoojasriMatti <srip6370@gmail.com>
2025-06-26 15:56:03 +0530

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,14 +1,12 @@
 package assignment;
-import java.util.Scanner;
 public class AreaOfCircle {
 	public static void main(String[] args) {
 		double pi = Math.PI;
-     Scanner s1 = new Scanner(System.in);
-     System.out.println("enter radius = ");
-        float r = s1.nextFloat();
+                 double r = 24.55;
+       
         double area = pi * r * r;
         System.out.println("area of circle = "+ area);
-        s1.close();
+        
      
      
 	}
